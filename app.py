ba7933e2b0f6cd87fdac44f430f323a5bf5ec578
steve2700 <stevnyaru@github.com>
2023-08-28 22:23:49 +0200

    run

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def  kevinstewart():
+    return "<h1>kevinstewart translator web app</h1>" 
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000)
+
+    
+    
