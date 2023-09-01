62e6e16d9550529a1149ae1d7a9d6ee52d4519e4
attam kevin <kevin.ejikez@gmail.com>
2023-09-01 05:09:08 -0700

    fixed

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,16 @@
-from flask import render_template
+from flask import render_template, flash
 from app import app
 from app.forms import TranslateForm
-from googletrans import translator
+from googletrans import Translator  # Corrected the import statement
 
-
-# ...
-
-@app.route('/translate')
-def Translate():
+@app.route('/translate', methods=['GET', 'POST'])  # Added 'POST' to the methods
+def translate():  # Renamed the function to lowercase
     form = TranslateForm()
     if form.validate_on_submit():
-        flash('Translation requested please wait')
-        translator = Translator()
-        translated=translator.translate(“translated_text”,dest=’target_language’)
-        return redirect('/index.html')
+        flash('Translation requested. Please wait.')  # Fixed the flash message
+        translator_instance = Translator()  # Renamed the variable to avoid conflicts
+        translated = translator_instance.translate(form.translated_text.data, dest=form.target_language.data)  # Fixed form attribute access
+        return translated.text
+
     return render_template('index.html', title='Translate', form=form)
+
