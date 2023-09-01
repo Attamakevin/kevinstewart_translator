9119af09e9d70e38c81dba49608c8d57f84ab0e7
Stewart Nyaruwata <112034196+steve2700@users.noreply.github.com>
2023-09-01 11:16:11 +0200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,23 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import TranslateForm
-from googletrans import translator
+from app import translate_text  # Import the function from app module
 
-
-# ...
-
-@app.route('/translate')
-def Translate():
+@app.route('/', methods=['GET', 'POST'])
+def home():
     form = TranslateForm()
+    message = None
+
     if form.validate_on_submit():
-        flash('Translation requested please wait')
-        translator = Translator()
-        translated = TRANSLATOR.translate("translated_text", dest='target_language')
-        return redirect('/index.html')
-    return render_template('index.html', title='Translate', form=form)
+        text = form.Translate_text.data
+        target_language = form.target_language.data
+        result = translate_text(text, target_language)
+        
+        if result['success']:
+            flash('Translation successful.')
+            message = f"Translation: {result['result']}"
+        else:
+            flash('Translation failed. Please try again later.')
+
+    return render_template('index.html', title='Translate', form=form, message=message)
+
