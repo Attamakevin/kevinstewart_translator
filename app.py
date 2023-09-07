96a07859996e4a612cdd26d18efca9aed496f0d1
attamakevin <kevinejikez@gmail.com>
2023-09-07 22:23:47 +0100

    text to voice implemented

--- app.py
+++ app.py
@@ -1,29 +1,58 @@
-from flask import render_template, Flask, flash, request
-from forms import TranslateForm
-from googletrans import Translator  # Corrected the import statement
-SECRET_KEY = 'URSECRET'
-app = Flask(__name__)
-@app.route('/', methods=['GET', 'POST'])  # Added 'POST' to the methods
-def translate():  # Renamed the function to lowercase
-    #return render_template('index.html')
+from flask import render_template, Flask, flash, request, jsonify, redirect
+from forms import TranslateForm  # You need to import the TranslateForm if it exists
+from googletrans import Translator
+from deep_translator import GoogleTranslator
+from flask_cors import cross_origin
+from models.speech import  text_to_speech
+from gtts import gTTS
+import os
 
-    #form = TranslateForm()
-    #if form.validate_on_submit():
-    #flash('Translation requested. Please wait.') # Fixed the flash message
+app = Flask(__name__)
+app.secret_key = 'YOUR_SECRET_KEY'  # Set your own secret key
+
+@app.route('/', methods=['GET', 'POST'])
+def translate():
+    translated_text = ""
     if request.method == 'POST':
         try:
-            text_to_translate = request.form["text-to-translate"].lower()
-            selected_language = request.form["select-language"]
-            translated_text = translator.translate(
-                text_to_translate, dest=selected_language)
-            text = translated_text.text
-            pronunciation_data = translated_text.pronunciation
-            if pronunciation_data is None:
-                pronunciation_data = "Sorry, data not available"
-            confidence = round((translated_text.extra_data["confidence"]) * 100, 2)
+            text_to_translate = request.form.get("text-to-translate").lower()
+            selected_language = request.form.get("select-language")
+
+            # Initialize the translator
+            my_translator = GoogleTranslator(source='auto', target= selected_language)
+
+            # Translate text
+            result = my_translator.translate(text=text_to_translate)
+
+            # Access the translated text
+            translated_text = result
+        
         except Exception as e:
-            pronunciation_data = "-"
-            text = "ERROR: We are not able to handle your request right now"
-            confidence = "-"
-        return render_template('index.html', translation_result=text, pronunciation=pronunciation_data, confidence_level=str(confidence) + " %")
-    return render_template('index.html')
+            print(e)
+            translated_text = "ERROR: We are not able to handle your request right now"
+
+    return render_template('index.html', translated_text=translated_text)
+
+@app.route('/voice', methods=['POST'])
+@cross_origin()
+def homepage():
+    if request.method == 'POST':
+
+        text = ""
+        text = request.form["translation-result"]
+        gender = request.form['voices']
+        print(text)
+        # Call your text_to_speech function here
+        text_to_speech(text,gender)
+            
+        # Redirect to the main page or render it with a success message
+        flash("Text converted to speech successfully!", 'success')
+        return redirect('/')
+    else:
+        print(gender)
+        print(e)
+        flash("Error converting text to speech", 'error')
+    
+    # Render the template with the translated text if needed
+    return render_template('index.html', text=text)
+
